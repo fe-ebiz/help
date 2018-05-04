88d4362f521bb84c8074917d7f2326b0dc3836ad
강혜준 <vxggangxv@naver.com>
2018-05-04 18:29:05 +0900

    메인 첫 페이지 제작

--- src/js/main/index.js
+++ src/js/main/index.js
@@ -4,93 +4,93 @@ $(function() {
 	mainVisualSliderFn();
 	
 	//서비스 리뷰 슬라이드
-    serviceSliderFn();
+	serviceSliderFn();
 });
 
 function mainVisualSliderFn() {
 	$('.mainIntro-sec .bxBox .bxS').bxSlider({
-        auto:true,
+		auto:true,
 		autoControls: true,
 		speed: 500,
 		duration: 6000,
-        nextText : '',
-        prevText : ''
-    });
+		nextText : '',
+		prevText : ''
+	});
 }
 
 function serviceSliderFn() {
-    var serviceSlider = $('#service-slider').bxSlider({
-        slideWidth : 'auto',
-        startSlide : 0,
-        moveSlides : 3,
-        minSlides : 3,
-        maxSlides : 3,
-        slideMargin : 10,
-        nextSelector : '#slider-next',
-        prevSelector : '#slider-prev',
-        nextText : '',
-        prevText : ''
-    });
-    
-    var wW = $(window).width();
-    
-    if( wW < 1100 ){
-        serviceSlider.reloadSlider({
-            slideWidth : 'auto',
-            startSlide : 0,
-            moveSlides : 1,
-            minSlides : 1,
-            maxSlides : 1,
-            slideMargin : 10,
-            nextSelector : '#slider-next',
-            prevSelector : '#slider-prev',
-            nextText : '',
-            prevText : ''
-        });
-    }else{
-        serviceSlider.reloadSlider({
-            slideWidth : 'auto',
-            startSlide : 0,
-            moveSlides : 3,
-            minSlides : 3,
-            maxSlides : 3,
-            slideMargin : 10,
-            nextSelector : '#slider-next',
-            prevSelector : '#slider-prev',
-            nextText : '',
-            prevText : ''
-        });
-    }
-    
-    $(window).resize(function(){
-        var wW = $(window).width();
+	var serviceSlider = $('#service-slider').bxSlider({
+		slideWidth : 'auto',
+		startSlide : 0,
+		moveSlides : 3,
+		minSlides : 3,
+		maxSlides : 3,
+		slideMargin : 10,
+		nextSelector : '#slider-next',
+		prevSelector : '#slider-prev',
+		nextText : '',
+		prevText : ''
+	});
+	
+	var wW = $(window).width();
+	
+	if( wW < 1100 ){
+		serviceSlider.reloadSlider({
+			slideWidth : 'auto',
+			startSlide : 0,
+			moveSlides : 1,
+			minSlides : 1,
+			maxSlides : 1,
+			slideMargin : 10,
+			nextSelector : '#slider-next',
+			prevSelector : '#slider-prev',
+			nextText : '',
+			prevText : ''
+		});
+	}else{
+		serviceSlider.reloadSlider({
+			slideWidth : 'auto',
+			startSlide : 0,
+			moveSlides : 3,
+			minSlides : 3,
+			maxSlides : 3,
+			slideMargin : 10,
+			nextSelector : '#slider-next',
+			prevSelector : '#slider-prev',
+			nextText : '',
+			prevText : ''
+		});
+	}
+	
+	$(window).resize(function(){
+		var wW = $(window).width();
 
-        if( wW < 1100 ){
-            serviceSlider.reloadSlider({
-                slideWidth : 'auto',
-                startSlide : 0,
-                moveSlides : 1,
-                minSlides : 1,
-                maxSlides : 1,
-                slideMargin : 10,
-                nextSelector : '#slider-next',
-                prevSelector : '#slider-prev',
-                nextText : '',
-                prevText : ''
-            });
-        }else{
-            serviceSlider.reloadSlider({
-                slideWidth : 'auto',
-                startSlide : 0,
-                moveSlides : 3,
-                minSlides : 3,
-                maxSlides : 3,
-                slideMargin : 10,
-                nextSelector : '#slider-next',
-                prevSelector : '#slider-prev',
-                nextText : '',
-                prevText : ''
-            });
-        }
-    })
+		if( wW < 1100 ){
+			serviceSlider.reloadSlider({
+				slideWidth : 'auto',
+				startSlide : 0,
+				moveSlides : 1,
+				minSlides : 1,
+				maxSlides : 1,
+				slideMargin : 10,
+				nextSelector : '#slider-next',
+				prevSelector : '#slider-prev',
+				nextText : '',
+				prevText : ''
+			});
+		}else{
+			serviceSlider.reloadSlider({
+				slideWidth : 'auto',
+				startSlide : 0,
+				moveSlides : 3,
+				minSlides : 3,
+				maxSlides : 3,
+				slideMargin : 10,
+				nextSelector : '#slider-next',
+				prevSelector : '#slider-prev',
+				nextText : '',
+				prevText : ''
+			});
+		}
+	})
 }
